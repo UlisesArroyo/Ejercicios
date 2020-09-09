79eb63c17d51e717a1cd25c4fd9b34b9d16942c9
UlisesArroyo <66101708+UlisesArroyo@users.noreply.github.com>
2020-09-09 12:26:09 -0500

    archivos python

---
+++ Python/deep_net_in_keras.py
@@ -0,0 +1,40 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout # new!
+from keras.layers.normalization import BatchNormalization # new!
+from keras.optimizers import SGD
+
+(X_train, y_train), (X_valid, y_valid) = mnist.load_data()
+
+X_train = X_train.reshape(60000, 784).astype('float32')
+X_valid = X_valid.reshape(10000, 784).astype('float32')
+
+X_train /= 255
+X_valid /= 255
+
+
+n_classes = 10
+y_train = keras.utils.to_categorical(y_train, n_classes)
+y_valid = keras.utils.to_categorical(y_valid, n_classes)
+
+model = Sequential()
+
+model.add(Dense(64, activation='relu', input_shape=(784,)))
+model.add(BatchNormalization())
+
+model.add(Dense(64, activation='relu'))
+model.add(BatchNormalization())
+
+model.add(Dense(64, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=128, epochs=20, verbose=1, validation_data=(X_valid, y_valid))
