21df50fe9a13043a6aaf9da5ce5bc7f1a118bda6
UlisesArroyo <ulisesdanarroyorojas@gmail.com>
2020-09-16 12:46:11 -0500

    mxnet_5

--- Mxnet/mxnet_cap_5.py
+++ Mxnet/mxnet_cap_5.py
@@ -1,49 +1,66 @@
 #version buena
 import mxnet as mx
-mnist = mx.test_utils.get_mnist()
-
-
-batch_size = 100
-train_iter = mx.io.NDArrayIter(mnist['train_data'], mnist['train_label'], batch_size, shuffle=True)
-val_iter = mx.io.NDArrayIter(mnist['test_data'], mnist['test_label'], batch_size)
+import logging
 
 
 
-data = mx.sym.var('data')
+#------------------------CAPTURA Y PROCESAMIENTO DE BASE DE DATOS-------------------------------
+mnist = mx.test_utils.get_mnist() #Descarga y carga las imagenes y las etiquetas en la memoria
+
+
+batch_size = 128				#Tamaño de los lotes
+train_iter = mx.io.NDArrayIter(mnist['train_data'], mnist['train_label'], batch_size, shuffle=True)#La base de datos se va a dividir de manera aleatoria en lotes de 128 tanto las imagenes como las y's
+val_iter = mx.io.NDArrayIter(mnist['test_data'], mnist['test_label'], batch_size)					#Lo mismo solo que se seccionara de manera ordenada
+#				ITERADORES
+
+
+data = mx.sym.var('data') #Crea la variable que sera modifcada pero no se si carga la informacion de train_iter , val_iter o de los 2
+
 # Flatten the data from 4-D shape into 2-D (batch_size, num_channel*width*height)
-data = mx.sym.flatten(data=data)
+data = mx.sym.flatten(data=data) #Aplana nuestras imagenes de 28 * 28 a 784
 
+#------------------------CAPTURA Y PROCESAMIENTO DE BASE DE DATOS-------------------------------
+
+
+
+
+#------------------------CONSTRUCCION DE RED NEURONAL-------------------------------
 
 # The first fully-connected layer and the corresponding activation function
-fc1  = mx.sym.FullyConnected(data=data, num_hidden=128)
-act1 = mx.sym.Activation(data=fc1, act_type="relu")
+fc1  = mx.sym.FullyConnected(data=data, num_hidden=128) #Se crea la capa densa y se especifica el numero de neuronas que tendra. Tambien especifica que informacion se le introducira
+act1 = mx.sym.Activation(data=fc1, act_type="sigmoid")		#Se especifica el tipo de funcion de activacion que tendra la primera capa
 
 # The second fully-connected layer and the corresponding activation function
-fc2  = mx.sym.FullyConnected(data=act1, num_hidden = 64)
-act2 = mx.sym.Activation(data=fc2, act_type="relu")
+#fc2  = mx.sym.FullyConnected(data=act1, num_hidden = 64)
+#act2 = mx.sym.Activation(data=fc2, act_type="relu")
 
 
 # MNIST has 10 classes
-fc3  = mx.sym.FullyConnected(data=act2, num_hidden=10)
+fc3  = mx.sym.FullyConnected(data=act1, num_hidden=10)	#Ultima capa 
 # Softmax with cross entropy loss
-mlp  = mx.sym.SoftmaxOutput(data=fc3, name='softmax')
+mlp  = mx.sym.SoftmaxOutput(data=fc3, name='softmax')	#La ultima funcion se especifica, en este caso softmax
 
-import logging
+#------------------------CONSTRUCCION DE RED NEURONAL-------------------------------
+
+
 logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
 # create a trainable module on CPU
 mlp_model = mx.mod.Module(symbol=mlp, context=mx.cpu())
-mlp_model.fit(train_iter,  # train data
-              eval_data=val_iter,  # validation data
-              optimizer='sgd',  # use SGD to train
-              optimizer_params={'learning_rate':0.1},  # use fixed learning rate
-              eval_metric='acc',  # report accuracy during training
-              batch_end_callback = mx.callback.Speedometer(batch_size, 100), # output progress for each 100 data batches
-              num_epoch=10)  # train for at most 10 dataset passes
+mlp_model.fit(train_iter,  # train data // Info de entrenamiento
+              eval_data=val_iter,  # validation data // Info de validacion
+              optimizer='sgd',  # use SGD to train // Tipo de optimizador a utilizar
+              optimizer_params={'learning_rate':0.1},  # use fixed learning rate // La taza de aprendizaje 
+              eval_metric='acc',  # report accuracy during training // reporte de exactitud durante el entrenamiento
+              batch_end_callback = mx.callback.Speedometer(batch_size, 100), # output progress for each 100 data batches // progreso por cada 100 lotes
+              num_epoch=10)  # train for at most 10 dataset passes // cantidad de epocas 
 
-test_iter = mx.io.NDArrayIter(mnist['test_data'], None, batch_size)
+
+#Realiza una prediccion a la probabilidad que tendra la red neuronal en determinar el numero de la imagen correctamente
+test_iter = mx.io.NDArrayIter(mnist['test_data'], None, batch_size) 
 prob = mlp_model.predict(test_iter)
 assert prob.shape == (10000, 10)
 
+#Se calcula la precision que tuvo la red neuronal 
 test_iter = mx.io.NDArrayIter(mnist['test_data'], mnist['test_label'], batch_size)
 # predict accuracy of mlp
 acc = mx.metric.Accuracy()
